e3b20a23e698b9b612a9cd104ad5a331b13afa94
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2024-06-26 16:00:22 +0500

    upd

---
+++ problem-forge-ai/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import ProblemStructure from "@/components/component/problem-structure";
+
+export default function Home() {
+  return (
+    <>
+      <ProblemStructure />
+    </>
+  );
+}
