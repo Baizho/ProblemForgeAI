716cd1fdef9a83f91f7168a3fe3a244367f06a45
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2024-07-24 18:59:57 +0500

    upd

---
+++ backend/install-python-dependencies.js
@@ -0,0 +1,13 @@
+const { exec } = require('child_process');
+
+exec('pip install sys random uuid', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error installing Python dependencies: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`Error: ${stderr}`);
+        return;
+    }
+    console.log(`Output: ${stdout}`);
+});
